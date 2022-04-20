6039aa7b85d37c9c0868e368d70e9ad79b60c12c
Jonathan Mayer <opaquefocus@gmail.com>
2022-04-20 16:22:29 -0600

    Add index.html

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+function computerPlay(){
+    let choices = ['Rock', 'Paper','Scissors']
+    return choices[Math.floor(Math.random()*choices.length)];
+}
+
+// console.log(computerPlay());
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+     
+    if (computerSelection == 'Paper'){
+        return ("You Lose")
+            }
+    
+    else if (computerSelection == 'Scissors'){
+        return ("You Win")
+    }
+    
+    else{
+        return ("You Tie")
+    }
+  }
+  
+  console.log("Computer Chooses: " +computerPlay());
+  console.log("Player Chooses: " +playerSelection);
+
+
+  console.log(playRound(playerSelection, computerSelection));
